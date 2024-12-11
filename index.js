23bad78db4db23868ff12ff46ab5a53aa840752c
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2024-12-12 05:39:45 +0600

    delete api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 8000;
 
@@ -44,6 +44,12 @@ async function run() {
     })
 
 
+    app.delete('/lesson/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await languageCollection.deleteOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
