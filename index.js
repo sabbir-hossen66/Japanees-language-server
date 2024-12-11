bcf036f0f8a08063c20c2a89dd7456667bf9bb0b
sabbir-hossen66 <sabbir.ms2000@gmail.com>
2024-12-12 04:52:12 +0600

    created post method for showing database

--- index.js
+++ index.js
@@ -27,12 +27,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const languageCollection = client.db('languageDB').collection('language')
+
+    app.post('/lesson', async (req, res) => {
+      const newLesson = req.body;
+      console.log(newLesson)
+      const result = await languageCollection.insertOne(newLesson);
+      res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
